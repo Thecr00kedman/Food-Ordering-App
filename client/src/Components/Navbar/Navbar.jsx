374d035dcb4fe96756a9dda032eba397e83c20e0
Aditya Sharma <117864728+Thecr00kedman@users.noreply.github.com>
2023-10-21 03:50:54 +0530

    fixed responsiveness

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -17,13 +17,22 @@ import FoodBankIcon from '@mui/icons-material/FoodBank';
 import HomeMaxIcon from '@mui/icons-material/HomeMax';
 import AddIcon from '@mui/icons-material/Add';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
+import { useEffect, useState } from "react";
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 export const Navbar = () => {
     const navigate= useNavigate()
     const [cookies, setCookies, removeCookies] = useCookies(["access_token"]);
     
+    const width= window.innerWidth
 
+    const[systemWidth,setSystemWidth]= useState(width)
+    useEffect(()=>{
+       setSystemWidth(width)
+    },width);
+    const[toggle,setToggle]= useState(false)
+    
 
     const handleLogout = ()=> {
           toast.success('Logged Out successfully')
@@ -33,9 +42,6 @@ export const Navbar = () => {
           navigate('/')
           setAccount(false)
           setPartner(false)
-          
-         
-
     }
 
 
@@ -44,7 +50,12 @@ export const Navbar = () => {
 
     return(
 
-        <Nav>
+        <>
+        {systemWidth<520?
+        (
+            toggle ? 
+            (
+                <Nav>
            <LogoContainer>
             <Box sx={{width:"9rem"}}><Link to={'/'}><img src={Logo} alt="Logo" /></Link></Box>            
             </LogoContainer>
@@ -89,7 +100,64 @@ export const Navbar = () => {
             </Cart>
             <Logout>{(account || partner )?<Button onClick={handleLogout} to={'/'}>{<PowerSettingsNewIcon/>}</Button> : <Button style={{display:'none'}}>{<PowerSettingsNewIcon/>}</Button>}</Logout>
         </Nav>
+            )
+            :
+           (
+            <MenuIcon/>
+           )
+        
+ 
+        ):(
+          <Nav>
+           <LogoContainer>
+            <Box sx={{width:"9rem"}}><Link to={'/'}><img src={Logo} alt="Logo" /></Link></Box>            
+            </LogoContainer>
+        
+            
+            <SearchBar>
+             {
+        
+               account?<Box><input type="text" placeholder="search your food here"/>&nbsp;&nbsp;<SearchIcon sx={{cursor:'pointer'}}/></Box> 
+                :
+                 (partner ?<Link to={'/ShowAll'}><HomeMaxIcon/>&nbsp;&nbsp;<span>Show All</span></Link>:<Box><input type="text"  placeholder="search your food here"/><SearchIcon/></Box>)
 
+             }
+            </SearchBar>
+            
+            <Offers>
+            {partner ?<Link to={'/RestProfile'}><AddIcon/>&nbsp;&nbsp;<span>Add Product</span></Link>:<Link ><LocalOfferIcon/>&nbsp;&nbsp;<span>Offers</span></Link>
+            }    </Offers>
+           
+            <Help>
+               <Link><CatchingPokemonIcon/>&nbsp;&nbsp;<span>Help</span></Link>
+            </Help>
+            <SignIn>
+                
+                {
+                (account) ?<Profile account={account} setAccount={setAccount} />:partner?(<Profile partner={partner} setPartner={setPartner}/>):<Link to={'/Login'}><PersonIcon/>&nbsp;&nbsp;<span>Sign in</span></Link>
+                }
+            </SignIn>
+            <Seller>
+                {
+               (account)?<Link to={'/PreviousOrder'}><FastfoodIcon/>&nbsp;&nbsp;<span>Orders</span></Link>:(!partner || account)?<Link to={"/RestSignup"}><StorefrontIcon/>&nbsp;&nbsp;<span>Partner</span></Link>:<Link to={'/PreviousOrder'} style={{display:'none'}}><FastfoodIcon/>&nbsp;&nbsp;<span>Orders</span></Link>
+                }            
+            </Seller>
+            <Cart>
+                {
+                    account ?<Link to={'/Cart'}><AddShoppingCartIcon/>&nbsp;&nbsp;<span>Cart</span></Link> 
+                    :
+                    (partner? <Link to={'/RestaurantOrder'}><FoodBankIcon/>&nbsp;&nbsp;<span>Orders</span></Link>
+                    :<Link to={'/Cart'}><AddShoppingCartIcon/>&nbsp;&nbsp;<span>Cart</span></Link>
+                    )
+                }            
+            </Cart>
+            <Logout>{(account || partner )?<Button onClick={handleLogout} to={'/'}>{<PowerSettingsNewIcon/>}</Button> : <Button style={{display:'none'}}>{<PowerSettingsNewIcon/>}</Button>}</Logout>
+        </Nav>
+  
+        )
+
+        }
+        </>
     )
 
 
